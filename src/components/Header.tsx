fa21855ef9eac5c3c1e840b59d9b611956078efa
Annatar <itsannatar@gamil.com>
2024-04-29 17:01:16 +0330

    feat: add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import { Flex, Image, Title } from "@mantine/core";
+
+const Header = () => {
+  return (
+    <Flex justify="space-between" align="center">
+      <Flex>
+        <Title order={1}>AM</Title>
+        <Title order={1} c="#FF5631">
+          TODO
+        </Title>
+      </Flex>
+      <Image w="4rem" h="4rem" src="/icons/react.svg" />
+    </Flex>
+  );
+};
+
+export default Header;
