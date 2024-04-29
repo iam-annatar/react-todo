186acd9b6efe53fb3fa6e2cc813f6b36623bac6a
Annatar <itsannatar@gamil.com>
2024-04-29 18:12:23 +0330

    feat: add custom theme

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import { theme } from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <MantineProvider
+      theme={theme}
       defaultColorScheme="dark"
       cssVariablesResolver={(t) => ({
         light: {},
