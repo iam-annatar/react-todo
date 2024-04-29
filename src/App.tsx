e7bb72a7d1e955b5ba653ac97b03c5f4cf1722ff
Annatar <itsannatar@gamil.com>
2024-04-29 18:37:58 +0330

    feat: add static stats component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Container } from "@mantine/core";
 
 import Header from "./components/Header";
+import Stats from "./components/Stats";
 
 const App = () => {
   return (
     <Container mt="xl" size="xl">
       <Header />
+      <Container mt="xl" size="xs">
+        <Stats />
+      </Container>
     </Container>
   );
 };
