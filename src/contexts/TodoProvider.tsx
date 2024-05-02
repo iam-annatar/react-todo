8e844f2d410de0c1edac728545046018cfe90776
Annatar <itsannatar@gamil.com>
2024-05-02 15:05:46 +0330

    feat: presist data with local storage

--- src/contexts/TodoProvider.tsx
+++ src/contexts/TodoProvider.tsx
@@ -1,47 +1,19 @@
-import { createContext, useMemo, useReducer, useState } from "react";
+import { createContext, useEffect, useMemo, useReducer, useState } from "react";
 
-import { ACTIONS } from "../constants";
-import type {
-  Action,
-  TodoContextType,
-  TodoItems,
-  TodoProviderProps,
-} from "../types";
+import { initilizer, todoReducer } from "../reducers";
+import type { TodoContextType, TodoProviderProps } from "../types";
 
 export const Context = createContext<TodoContextType>({} as TodoContextType);
 
-const todoReducer = (todos: TodoItems[], action: Action): TodoItems[] => {
-  const { type, payload } = action;
-
-  switch (type) {
-    case ACTIONS.ADD:
-      if (payload) {
-        return [...todos, payload];
-      }
-
-      return todos;
-
-    case ACTIONS.DELETE:
-      return todos.filter((todo) => todo.id !== payload?.id);
-
-    case ACTIONS.EDIT:
-      return todos.map((todo) =>
-        todo.id === payload?.id ? { ...todo, ...payload } : todo,
-      );
-
-    case ACTIONS.TOGGLE_COMPLETE:
-      return todos.map((todo) =>
-        todo.id === payload?.id ? { ...todo, ...payload } : todo,
-      );
-
-    default:
-      return todos;
-  }
-};
-
 export const TodoProvider = ({ children }: TodoProviderProps) => {
-  const [todos, dispatch] = useReducer(todoReducer, []);
+  const [todos, dispatch] = useReducer(todoReducer, [], initilizer);
   const [inputValue, setInputValue] = useState<string>("");
+
+  useEffect(() => {
+    if (todos) {
+      localStorage.setItem("Todos", JSON.stringify(todos));
+    }
+  }, [todos]);
 
   const values = useMemo(() => {
     return {
